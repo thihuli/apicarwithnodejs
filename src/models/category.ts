c2ed0f0ec13cce6425d40e047efece39897464d1
thihuli <thiagocandidof@hotmail.com>
2022-06-13 18:35:43 -0300

    feat: add model category

---
+++ src/models/category.ts
@@ -0,0 +1,8 @@
+class Category {
+  name: string;
+  id: string;
+  description: string;
+  created_at: Date;
+}
+
+export { Category };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
+import { Category } from '../models/category';
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
+  const category: Category = {
     name,
     description,
     id: uuidv4(),
