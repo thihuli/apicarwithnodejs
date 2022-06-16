6463ad592fb2aa69b0ad36ee9882dbda7bee1dd0
thihuli <thiagocandidof@hotmail.com>
2022-06-16 18:31:24 -0300

    fix: removing call class

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,12 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../repositories/categoriesRepository';
-import { CreateCategoryService } from '../services/createCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
+categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response));
 
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-  return response.status(201).send();
-});
-
-categoriesRoutes.get('/', (request, response) => {
-  const allCategories = categoriesRepository.list();
-
-  return response.json(allCategories);
-});
+categoriesRoutes.get('/', (request, response) => listCategoriesController.handle(request, response));
 
 export { categoriesRoutes };
