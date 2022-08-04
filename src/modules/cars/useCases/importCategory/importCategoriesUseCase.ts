97ebeed6a4329bf848ec381805e402bfdb50d065
thihuli <thiagocandidof@hotmail.com>
2022-08-03 22:27:02 -0300

    updates

--- src/modules/cars/useCases/importCategory/importCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoriesUseCase.ts
@@ -26,6 +26,7 @@ class ImportCategoryUseCase {
           name, description,
         });
       }).on('end', () => {
+        fs.promises.unlink(file.path)
         resolve(categories);
       })
         .on('error', (err) => {
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import swaggerUi from "swagger-ui-express"
+import swaggerFile from "./swagger.json";
 
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve);
+app.use("/docs", swaggerUi.setup(swaggerFile))
 
 app.use(router);
 
